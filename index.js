28ba794196e589ace2b08540d42d64062221bfd7
Adam Tann <adamtann21@gmail.com>
2020-04-02 04:42:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string = "I love you, Grandma."; {
-    console.log("I love you, too")
-  } if string === lowercase ; {
-    console.log('I can\'t hear you') ;
-  } if string === uppercase ; {
-    console.log('YES INDEED');
+  if (string === 'I love you, Grandma.') {
+    return "I love you, too."
   }
-}
+  if (string.toLowerCase() === string) {
+    return "I can\'t hear you"
+  } 
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
