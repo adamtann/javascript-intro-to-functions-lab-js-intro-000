fb91281b2a05d5c84e6f87a7927cc72c9fe14f4d
Adam Tann <adamtann21@gmail.com>
2020-04-02 04:43:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I love you, too."
   }
   if (string.toLowerCase() === string) {
-    return "I can\'t hear you"
+    return "I can\'t hear you."
   }
   if (string.toUpperCase() === string) {
     return "YES INDEED!"
